498a7b18310b666799d0d2e7d0a700676eb850f7
Jando <alexandroguterez@gmail.com>
2022-08-15 22:16:37 -0700

    Make buttons to add work and education sections

--- src/SchoolFields.js
+++ src/SchoolFields.js
@@ -6,21 +6,29 @@ class SchoolFields extends Component {
     super(props);
 
     this.state = {
-     
+     children: 1
     };
-  }
 
+    this.addSection = this.addSection.bind(this);
+  }
+  addSection = () => {
+    this.setState({ children: this.state.children + 1});
+  }
   render() {
+    const children = [];
+    for (let i = 0; i < this.state.children; i++) {
+      children.push( <div className="SchoolSection" key={i}>
+      <DataField title="School Name" name="school" type="text" />
+      <DataField title="Major" name="major" type="text" />
+      <DataField title="Starting Date" name="schoolStart" type="date" />
+      <DataField title="Ending Date" name="schoolEnd" type="date" />
+    </div>);
+    }
     return (
       <div className="Section">
         <h1>Education</h1>
-        <div className="SchoolFields">
-          <DataField title="School Name" name="school" type="text" />
-          <DataField title="Major" name="major" type="text" />
-          <DataField title="Starting Date" name="schoolStart" type="date" />
-          <DataField title="Ending Date" name="schoolEnd" type="date" />
-        </div>
-        <div className="AddBtn">Add Education</div>
+          {children}
+        <div className="AddBtn" onClick={() => this.addSection()}>Add Education</div>
       </div>
     );
   }
