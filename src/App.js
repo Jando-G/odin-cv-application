0f84a7f4df1d21bb78339e775cc47dd6cc1baa8a
Jando <alexandroguterez@gmail.com>
2022-08-30 17:33:45 -0700

    Hard Reset Project

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import './styles.css';
-import MainInfoFields from './MainInfoFields';
-import CompanyFields from './CompanyFields.js';
-import SchoolFields from './SchoolFields.js';
-import Page from './Page';
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state= {
-      FirstName: '',
-      LastName: '',
-      Email: '',
-      PhoneNumber: ''
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange() {
-    
-  }
   render() {
     return (
       <div className="App">
-          <form>
-            <MainInfoFields/>
-            <CompanyFields />
-            <SchoolFields />
-          </form>
-          <Page />
+        Hello World
       </div>
     );
   }
